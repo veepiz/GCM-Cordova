e6d8fb73206f71a214c4ab33d05b72d9014de27b
Mark Nutter <marknutter@gmail.com>
2012-06-27 16:31:07 -0500

    initial upgrade for cordova compatibility

--- assets/www/PG_C2DM_script.js
+++ assets/www/PG_C2DM_script.js
@@ -6,122 +6,119 @@ gApp.c2dmregid = '';
 
 window.onbeforeunload  =  function(e) {
 
-		if ( gApp.c2dmregid.length > 0 )
-		{
-			// The same routines are called for success/fail on the unregister. You can make them unique if you like
-			window.plugins.C2DM.unregister( C2DM_Success, C2DM_Fail );			// close the C2DM 
-		
-		}
+    if ( gApp.c2dmregid.length > 0 )
+    {
+      // The same routines are called for success/fail on the unregister. You can make them unique if you like
+      window.plugins.C2DM.unregister( C2DM_Success, C2DM_Fail );      // close the C2DM
+
+    }
 };
 
 
 
-//$(document).bind("mobileinit", function() {
-//alert( 'Starting...');
 
-	$("#app-status-ul").append( '<li>Mobileinit event received' );
 
-	document.addEventListener('deviceready', function() {
-		// This is the PhoneGap deviceready event. Once this is called PhoneGap is available to be used
-		$("#app-status-ul").append( '<li>deviceready event received' );
+  alert('Mobileinit event received' );
 
-		$("#app-status-ul").append( '<li>calling C2DM.register, register our email with Google' );
+  document.addEventListener('deviceready', function() {
+    // This is the PhoneGap deviceready event. Once this is called PhoneGap is available to be used
+    alert('deviceready event received' );
 
-		
-		gApp.DeviceReady = true;
+    alert('calling C2DM.register, register our email with Google' );
 
-		// Some Unique stuff here,
-		// The first Parm is your Google email address that you were authorized to use C2DM with
-		// the Event Processing rountine (2nd parm) we pass in the String name
-		// not a pointer to the routine, under the covers a JavaScript call is made so the name is used
-		// to generate the function name to call. I didn't know how to call a JavaScript routine from Java
-		// The last two parms are used by PhoneGap, they are the callback routines if the call is successful or fails
-		//
-		// CHANGE: your_c2dm_account@gmail.com
-		// TO: what ever your C2DM authorized email account name is
-		//
-		window.plugins.C2DM.register("your_c2dm_account@gmail.com", "C2DM_Event", C2DM_Success, C2DM_Fail );
-		
-	}, false );
-	
-	
-//});
 
-function 
+    gApp.DeviceReady = true;
+
+    // Some Unique stuff here,
+    // The first Parm is your Google email address that you were authorized to use C2DM with
+    // the Event Processing rountine (2nd parm) we pass in the String name
+    // not a pointer to the routine, under the covers a JavaScript call is made so the name is used
+    // to generate the function name to call. I didn't know how to call a JavaScript routine from Java
+    // The last two parms are used by PhoneGap, they are the callback routines if the call is successful or fails
+    //
+    // CHANGE: your_c2dm_account@gmail.com
+    // TO: what ever your C2DM authorized email account name is
+    //
+    window.plugins.C2DM.register("your_c2dm_account@gmail.com", "C2DM_Event", C2DM_Success, C2DM_Fail );
+
+  }, false );
+
+
+function
 C2DM_Event(e)
 {
-	var li = '';
-	li = '<li>EVENT -> RECEIVED:' + e.event;
-	$("#app-status-ul").append( li )
-	
-	switch( e.event )
-	{
-	case 'registered':
-		// the definition of the e variable is json return defined in C2DMReceiver.java
-		// In my case on registered I have EVENT and REGID defined
-		gApp.c2dmregid = e.regid;
-		if ( gApp.c2dmregid.length > 0 )
-		{
-			li = '<li>REGISTERED -> REGID:' + e.regid;
-			$("#app-status-ul").append( li )
-			
-			// ==============================================================================
-			// ==============================================================================
-			// ==============================================================================
-			//
-			// This is where you would code to send the REGID to your server for this device
-			//
-			// ==============================================================================
-			// ==============================================================================
-			// ==============================================================================
-			
-		}
-			
-		break
-		
-	case 'message':
-		// the definition of the e variable is json return defined in C2DMReceiver.java
-		// In my case on registered I have EVENT, MSG and MSGCNT defined
-			
-		// You will NOT receive any messages unless you build a HOST server application to send 
-		// Messages to you, This is just here to show you how it might work
-			
-		li = '<li>MESSAGE -> MSG:' + e.msg;
-		$("#app-status-ul").append( li )
-		li = '<li>MESSAGE -> MSGCNT:' + e.msgcnt;
-		$("#app-status-ul").append( li )
 
-		break;
+  alert('EVENT -> RECEIVED:' + e.event);
 
-		
-	case 'error':
-			
-		li = '<li>ERROR -> MSG:' + e.msg;
-		$("#app-status-ul").append( li )
-		break;
-		
-		
-		
-	default:
-		li = '<li>EVENT -> Unknown, an event was received and we do not know what it is';
-		$("#app-status-ul").append( li )
-		break;
-	}
+
+  switch( e.event )
+  {
+  case 'registered':
+    // the definition of the e variable is json return defined in C2DMReceiver.java
+    // In my case on registered I have EVENT and REGID defined
+    gApp.c2dmregid = e.regid;
+    if ( gApp.c2dmregid.length > 0 )
+    {
+      alert('REGISTERED -> REGID:' + e.regid);
+
+
+      // ==============================================================================
+      // ==============================================================================
+      // ==============================================================================
+      //
+      // This is where you would code to send the REGID to your server for this device
+      //
+      // ==============================================================================
+      // ==============================================================================
+      // ==============================================================================
+
+    }
+
+    break
+
+  case 'message':
+    // the definition of the e variable is json return defined in C2DMReceiver.java
+    // In my case on registered I have EVENT, MSG and MSGCNT defined
+
+    // You will NOT receive any messages unless you build a HOST server application to send
+    // Messages to you, This is just here to show you how it might work
+
+    alert('MESSAGE -> MSG:' + e.msg);
+
+    alert('MESSAGE -> MSGCNT:' + e.msgcnt);
+
+
+    break;
+
+
+  case 'error':
+
+    alert('ERROR -> MSG:' + e.msg);
+
+    break;
+
+
+
+  default:
+    alert('EVENT -> Unknown, an event was received and we do not know what it is');
+
+    break;
+  }
 }
 
-function 
+function
 C2DM_Success(e)
 {
-	li = '<li>C2DM_Success -> We have successfully registered and called the C2DM plugin, waiting for C2DM_Event:reistered -> REGID back from Google';
-	$("#app-status-ul").append( li )
+  alert('C2DM_Success -> We have successfully registered and called the C2DM plugin, waiting for C2DM_Event:reistered -> REGID back from Google');
+
 }
 
-function 
+function
 C2DM_Fail(e)
 {
-	li = '<li>C2DM_Fail -> C2DM plugin failed to register';
-	$("#app-status-ul").append( li )
-	li = '<li>C2DM_Fail -> ' + e.msg;
-	$("#app-status-ul").append( li )
+  alert('C2DM_Fail -> C2DM plugin failed to register');
+
+  alert('C2DM_Fail -> ' + e.msg);
+
 }
 
--- src/com/plugin/C2DM/C2DMPlugin.java
+++ src/com/plugin/C2DM/C2DMPlugin.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.plugin.C2DM;
 
@@ -19,9 +19,9 @@ import org.json.JSONObject;
 import android.util.Log;
 
 import com.google.android.c2dm.C2DMessaging;
-import com.phonegap.api.Plugin;
-import com.phonegap.api.PluginResult;
-import com.phonegap.api.PluginResult.Status;
+import org.apache.cordova.api.Plugin;
+import org.apache.cordova.api.PluginResult;
+import org.apache.cordova.api.PluginResult.Status;
 
 
 /**
@@ -31,84 +31,84 @@ import com.phonegap.api.PluginResult.Status;
 
 public class C2DMPlugin extends Plugin {
 
-	public static final String ME="C2DMPlugin";	
+  public static final String ME="C2DMPlugin";
 
-	public static final String REGISTER="register";	
-	public static final String UNREGISTER="unregister";	
-	
-	public static Plugin gwebView;
-	private static String gECB;
-	private static String gEMail;
-	
-	@Override
-	public PluginResult execute(String action, JSONArray data, String callbackId) 
-	{
+  public static final String REGISTER="register";
+  public static final String UNREGISTER="unregister";
 
-		PluginResult result = null;
-		
-		Log.v(ME + ":execute", "action=" + action);		
-		
-		if (REGISTER.equals(action)) {
-			
-			Log.v(ME + ":execute", "data=" + data.toString());		
-			
-			try {				
-				
-				JSONObject jo= new JSONObject(data.toString().substring(1, data.toString().length()-1));
-				
-				gwebView = this;
+  public static Plugin gwebView;
+  private static String gECB;
+  private static String gEMail;
 
-				Log.v(ME + ":execute", "jo=" + jo.toString());		
-				
-				gECB = (String)jo.get("ecb");
-				gEMail = (String)jo.get("email");
-				
-				Log.v(ME + ":execute", "ECB="+gECB+" EMail="+gEMail );
-				
-				C2DMessaging.register(this.ctx, gEMail);
+  @Override
+  public PluginResult execute(String action, JSONArray data, String callbackId)
+  {
 
-				
-				Log.v(ME + ":execute", "C2DMessaging.register called ");
-			
-				result = new PluginResult(Status.OK);
-			} 
-			catch (JSONException e) {
-				Log.e(ME, "Got JSON Exception "
-					+ e.getMessage());
-				result = new PluginResult(Status.JSON_EXCEPTION);
-			}
-		}
-		else if (UNREGISTER.equals(action)) {
-			
-			C2DMessaging.unregister(this.ctx);
-			Log.v(ME + ":" + UNREGISTER, "C2DMessaging.unregister called ");
-			
-		}
-		else 
-		{
-			result = new PluginResult(Status.INVALID_ACTION);
-			Log.e(ME, "Invalid action : "+action);
-		}
-		
-		return result;		
-	}
-	
-	
-	public static void sendJavascript( JSONObject _json )
-	{
-		String _d =  "javascript:"+gECB+"(" + _json.toString() + ")";
- 	   	Log.v(ME + ":sendJavascript", _d);
- 	   	
- 	   	gwebView.sendJavascript( _d );
-	}
+    PluginResult result = null;
+
+    Log.v(ME + ":execute", "action=" + action);
+
+    if (REGISTER.equals(action)) {
+
+      Log.v(ME + ":execute", "data=" + data.toString());
+
+      try {
+
+        JSONObject jo= new JSONObject(data.toString().substring(1, data.toString().length()-1));
+
+        gwebView = this;
+
+        Log.v(ME + ":execute", "jo=" + jo.toString());
+
+        gECB = (String)jo.get("ecb");
+        gEMail = (String)jo.get("email");
+
+        Log.v(ME + ":execute", "ECB="+gECB+" EMail="+gEMail );
+
+        C2DMessaging.register(this.ctx.getContext(), gEMail);
 
 
-	/**
-	 * Gets the Directory listing for file, in JSON format
-	 * @param file The file for which we want to do directory listing
-	 * @return JSONObject representation of directory list. e.g {"filename":"/sdcard","isdir":true,"children":[{"filename":"a.txt","isdir":false},{..}]}
-	 * @throws JSONException
-	 */
-	
-	
+        Log.v(ME + ":execute", "C2DMessaging.register called ");
+
+        result = new PluginResult(Status.OK);
+      }
+      catch (JSONException e) {
+        Log.e(ME, "Got JSON Exception "
+          + e.getMessage());
+        result = new PluginResult(Status.JSON_EXCEPTION);
+      }
+    }
+    else if (UNREGISTER.equals(action)) {
+
+      C2DMessaging.unregister(this.ctx.getContext());
+      Log.v(ME + ":" + UNREGISTER, "C2DMessaging.unregister called ");
+
+    }
+    else
+    {
+      result = new PluginResult(Status.INVALID_ACTION);
+      Log.e(ME, "Invalid action : "+action);
+    }
+
+    return result;
+  }
+
+
+  public static void sendJavascript( JSONObject _json )
+  {
+    String _d =  "javascript:"+gECB+"(" + _json.toString() + ")";
+        Log.v(ME + ":sendJavascript", _d);
+
+        gwebView.sendJavascript( _d );
+  }
+
+
+  /**
+   * Gets the Directory listing for file, in JSON format
+   * @param file The file for which we want to do directory listing
+   * @return JSONObject representation of directory list. e.g {"filename":"/sdcard","isdir":true,"children":[{"filename":"a.txt","isdir":false},{..}]}
+   * @throws JSONException
+   */
+
+
 }
